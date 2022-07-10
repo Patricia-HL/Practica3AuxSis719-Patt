ebafd5d5bd36931af616de82e3cf6b57209c5031
Patricia-HL <phuizalopez1@gmail.com>
2022-07-10 20:03:58 +0000

    Patricia Huiza Lopez CI:8505629

---
+++ src/components/about/About.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import About from "./About";
+export default {
+  title: "Patricia-Huiza-Lopez/app/about",
+  component: About,
+};
+
+const Template = (args) => <About {...args} />;
+export const about= Template.bind({});
